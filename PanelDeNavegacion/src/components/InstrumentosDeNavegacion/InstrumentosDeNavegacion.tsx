e1dbc9def43ffa0c7b9169aa92e45f6ee0836e87
Altamiran0 <altamirano.41avc@gmail.com>
2025-05-23 20:39:36 -0300

    Comienzo del desarrollo de la web

---
+++ PanelDeNavegacion/src/components/InstrumentosDeNavegacion/InstrumentosDeNavegacion.tsx
@@ -0,0 +1,9 @@
+function InstrumentosDeNavegacion() {
+   return(
+      <div>
+         <canvas id="Panel" width="600" height="600" />
+      </div>
+   )
+};
+
+export default InstrumentosDeNavegacion;
---
+++ PanelDeNavegacion/src/components/index.ts
@@ -0,0 +1,1 @@
+export { default as InstrumentosDeNavegacion } from './InstrumentosDeNavegacion/InstrumentosDeNavegacion';
