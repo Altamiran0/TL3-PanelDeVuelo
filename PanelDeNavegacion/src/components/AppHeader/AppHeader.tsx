efb63196561585c1d3c3ee33b0fbdbe34af68add
Altamiran0 <altamirano.41avc@gmail.com>
2025-06-27 14:48:20 -0300

    Interfaz completada

--- PanelDeNavegacion/src/components/AppHeader/AppHeader.tsx
+++ PanelDeNavegacion/src/components/AppHeader/AppHeader.tsx
@@ -1,16 +1,20 @@
 import './AppHeader.css';
 
 function AppHeader () {
-   const isConnected = false;
+   const isConnected = true;
+   const GPSmodule_isWork = false
    return (
    <header className={ "appHeader" }>
       <h1>Panel de Navegacion</h1>
       <h3>Instrumentos de Navegacion a tiempo real</h3>
-      {  isConnected
-         ? <p className={ "textState" }>
-            <span className={ "state isConnected" }/> Conectado </p>
-         : <p className={ "textState" }>
+      {  ! isConnected
+         ?  <p className={ "textState" }>
             <span className={ "state noConnected" }/> No conectado </p>
+         : GPSmodule_isWork 
+            ?  <p className={ "textState" }>
+               <span className={ "state isConnected" }/> Conectado </p>
+            :  <p className={ "textState" }>
+               <span className={ "state GPSmoduleDontWork" }/> Conectado </p>
       }
    </header>
    );
--- PanelDeNavegacion/src/components/CurrentMap/CurrentMap.tsx
+++ PanelDeNavegacion/src/components/CurrentMap/CurrentMap.tsx
@@ -1,11 +1,42 @@
-import "./CurrentMap.css";
+import { useRef, useEffect } from 'preact/hooks';
+import L, { Map as LeafletMap } from 'leaflet';
+import '../../services/leaflet-config';
+import './CurrentMap.css';
 
 function CurrentMap() {
-return (
-   <div className={ "map" }>
-      <span className={ "positionCard" }>
-         <p>Latitud: { "-34.7242" }º</p>
-         <p>Longitud: { "-58.3506" }º</p>
+   const mapRef = useRef< HTMLDivElement >( null );
+   const leafletMapRef = useRef< LeafletMap >( null );
+
+   const position = { lat: -34.7242, lng: -58.3506 }
+   const zoom = 18;
+
+   useEffect(() => {
+    if ( !mapRef.current ) return;
+    if ( !leafletMapRef.current ) {
+      const map = L.map( mapRef.current, {
+        zoomControl: false
+      }).setView([ position.lat, position.lng ], zoom );
+      L.tileLayer( 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' ).addTo( map );
+      
+      L.marker([ position.lat, position.lng ]).addTo( map );
+      leafletMapRef.current = map;
+    } else 
+      leafletMapRef.current.setView([ position.lat, position.lng ], zoom);
+
+    return () => {
+      if (leafletMapRef.current) {
+        leafletMapRef.current.off('move');
+      }
+    };
+  }, [position.lat, position.lng, zoom]);
+
+   return (
+   <div className={ "mapContainer" }>
+      <div ref={ mapRef } className={ "map" }/>
+
+      <span className="positionCard">
+        <p>Latitud: { position.lat }º</p>
+        <p>Longitud: { position.lng }º</p>
       </span>
    </div>
    );
